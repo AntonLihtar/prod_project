f3f3dfe3f661e8a6e9c2e004d05ae0740c495de4
Antonl1 <sa-fa@mail.ru>
2024-02-07 00:41:27 +0300

    feat: add router config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,23 +5,19 @@ import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AboutPage } from "pages/AboutPage";
 import { MainPage } from "pages/MainPage";
+import { AppRouter } from "app/providers/router";
 
 
 const App = () => {
 
-    const {theme, toggleTheme} = useTheme();
+    const { theme, toggleTheme } = useTheme();
 
     return (
       <div className={classNames('app', {}, [theme])}>
           <button onClick={toggleTheme}>Toggle thame</button>
           <Link to={"/"}>Главная</Link>
           <Link to={"/about"}>О сайте</Link>
-          <Suspense fallback={<div>Loading...</div>}>
-              <Routes>
-                  <Route path={"/about"} element={<AboutPage/>}/>
-                  <Route path={"/"} element={<MainPage/>}/>
-              </Routes>
-          </Suspense>
+          <AppRouter/>
       </div>
     );
 };
